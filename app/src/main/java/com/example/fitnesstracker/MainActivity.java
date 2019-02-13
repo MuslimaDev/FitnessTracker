4c674d8e755f6f76defa4479ed575b36790e4cef
MuslimaDev <muslima1322@gmail.com>
2019-02-13 16:34:44 +0600

    now i have two movements between activities, added mapbox access key, created MapActivity and activity_map

--- app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.fitnesstracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.plusButton);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+        startActivity(new Intent(this, MapActivity.class));
     }
 }
