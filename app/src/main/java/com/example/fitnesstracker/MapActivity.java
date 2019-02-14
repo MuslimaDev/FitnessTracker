b837466c89968faf079cb572b839de53794d936a
MuslimaDev <muslima1322@gmail.com>
2019-02-14 10:38:37 +0600

    added "plus" button on the map

--- app/src/main/java/com/example/fitnesstracker/MapActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MapActivity.java
@@ -1,12 +1,78 @@
 package com.example.fitnesstracker;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity {
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    MapView mapView;
+    MapboxMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this,getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_map);
+        mapView = findViewById(R.id.mapView);
+        initMap(savedInstanceState);
+    }
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        MapActivity.this.map = mapboxMap;
+        mapboxMap.setStyle(Style.DARK);
+    }
+
+    private void initMap(Bundle savedInstanceState) {
+        mapView.getMapAsync(this);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle outState) {
+        super.onRestoreInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
